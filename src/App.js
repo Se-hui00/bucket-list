bd34360a6857d30ddace296459b79d5988b5cb0e
sehuishin <he0817kr@gamail.com>
2023-03-14 16:26:47 +0900

    과제제출용

---
+++ src/App.js
@@ -0,0 +1,163 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { StatusBar, Dimensions } from 'react-native'; //상태바
+import styled, { ThemeProvider } from 'styled-components/native';
+import { theme } from './theme';
+import Input from './components/Input';
+import Task from './components/Task';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SplashScreen from 'expo-splash-screen';
+
+//내용
+const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${({ theme }) => theme.background};
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+//제목
+const Title = styled.Text`
+  font-size: 40px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text};
+  margin: 0px 20px;
+`;
+
+const List = styled.ScrollView`
+  flex: 1;
+  width: ${({ width }) => width - 40}px;
+`;
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+const App = () => {
+  const width = Dimensions.get('window').width;
+
+  const [isReady, setIsReady] = useState(false);
+  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState({});
+
+  //저장
+  const _saveTasks = async tasks => {
+    try {
+      await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
+      setTasks(tasks);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  //불러오기
+  const _loadTasks = async () => {
+    try {
+      const loadedTasks = await AsyncStorage.getItem('tasks');
+      setTasks(JSON.parse(loadedTasks || {}));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await _loadTasks();
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (isReady) {
+  //     // This tells the splash screen to hide immediately! If we call this after
+  //     // `setAppIsReady`, then we may see a blank screen while the app is
+  //     // loading its initial state and rendering its first pixels. So instead,
+  //     // we hide the splash screen once we know the root view has already
+  //     // performed layout.
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
+  //추가기능
+  const _addTask = () => {
+    const ID = Date.now().toString();
+    const newTaskObject = {
+      [ID]: { id: ID, text: newTask, completed: false },
+    };
+    setNewTask('');
+    _saveTasks({ ...tasks, ...newTaskObject });
+  };
+
+  //삭제기능
+  const _deleteTask = id => {
+    const currentTasks = Object.assign({}, tasks);
+    delete currentTasks[id];
+    _saveTasks(currentTasks);
+  };
+
+  //완료시 체크기능
+  const _toggleTask = id => {
+    const currentTasks = Object.assign({}, tasks);
+    currentTasks[id]['completed'] = !currentTasks[id]['completed'];
+    _saveTasks(currentTasks);
+  };
+
+  //수정기능
+  const _updateTask = item => {
+    const currentTasks = Object.assign({}, tasks);
+    currentTasks[item.id] = item;
+    _saveTasks(currentTasks);
+  };
+
+  const _handleTextChange = text => {
+    setNewTask(text);
+  };
+
+  //입력 취소
+  const _onBlur = () => {
+    setNewTask('');
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={theme.background}
+        />
+        <Title>버킷 리스트</Title>
+        <Input
+          placeholder="+항목추가"
+          value={newTask}
+          onChangeText={_handleTextChange}
+          onSubmitEditing={_addTask}
+          onBlur={_onBlur}
+        />
+        <List width={width}>
+          {Object.values(tasks)
+            .reverse()
+            .map(item => (
+              <Task
+                key={item.id}
+                item={item}
+                deleteTask={_deleteTask}
+                toggleTask={_toggleTask}
+                updateTask={_updateTask}
+              />
+            ))}
+        </List>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+const theme = {
+  background: 'rgb(226, 149, 195)',
+  itemBackground: 'rgba(239, 232, 247, 0.6)',
+  main: 'rgb(83, 66, 76)',
+  text: 'rgb(255, 255, 255)',
+  done: 'rgb(186, 166, 177)',
+};
+
+export { theme };
